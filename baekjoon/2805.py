e705ea690eb15b5ac82db2320dcd0db8cd681904
WonJoonChoi <724thomas@gmail.com>
2023-01-20 02:47:39 +0900

    230119

--- baekjoon/2805.py
+++ baekjoon/2805.py
@@ -1,5 +1,14 @@
 # https://www.acmicpc.net/problem/2805
 
+
+'''
+1. 아이디어 :
+    1) 이분탐색 문제다. 최소1, 최대max(tree)로 해당 지점에서 잘랐을때의 합을 구하면서 탐색을 한다.
+2. 시간복잡도 :
+    1) Constructor: O(n), getRandom: O(1)
+3. 자료구조 :
+    1) 이분탐색
+'''
 import sys
 
 n, target = map(int, sys.stdin.readline().rstrip().split())
@@ -9,7 +18,6 @@ lp, rp = 1, max(alist)
 while lp<=rp:
     cmin = (lp + rp) // 2
     cut = sum([x-cmin for x in alist if x>cmin])
-
     if cut >= target:
         lp = cmin+ 1
     else:
