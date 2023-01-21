1223b982a597085491bceacb780170a7e304326c
WonJoonChoi <724thomas@gmail.com>
2023-01-21 22:03:20 +0900

    230121

--- LeetCode/100SameTree.py
+++ LeetCode/100SameTree.py
@@ -27,3 +27,26 @@ class Solution:
                 return False
             return dfs(node1.left,node2.left) and dfs(node1.right, node2.right)
         return dfs(p,q)
+
+    def isSameTree2(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def check(node1, node2):
+            if not node1 and not node2:
+                return True
+            if not node1 or not node2:
+                return False
+            if node1.val != node2.val:
+                return False
+            return True
+
+        deq = deque([(p,q)])
+        print(deq)
+        while deq:
+            p, q = deq.popleft()
+            if not check(p,q):
+                return False
+
+            if p:
+                deq.append((p.left,q.left))
+                deq.append((p.right,q.right))
+
+        return True
