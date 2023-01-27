dac32b4bdb952fcfe03bdc0c6597aaef224ad165
WonJoonChoi <724thomas@gmail.com>
2023-01-27 10:15:45 +0900

    230127

--- LeetCode/61RotateList.py
+++ LeetCode/61RotateList.py
@@ -1,5 +1,15 @@
 # https://leetcode.com/problems/rotate-list/description/
 
+'''
+1. 아이디어 :
+    1) 끝 노드와 첫 노드를 연결하고, k번째 노드를 찾아서 끊어준다.
+2. 시간복잡도 :
+    1) O(n) + O(n) = O(n)
+    - 끝 노드까지의 탐색 + k번째 노드까지의 탐색
+3. 자료구조 :
+    1) 링크드 리스트
+'''
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -9,12 +19,12 @@ class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if head==None or head.next==None:
             return head
-        curr=head
-        length=1
+        curr, length = head, 1
         while curr.next:
             curr=curr.next
             length+=1
         curr.next=head
+
         idel = length-(k%length)
         curr=head
         for i in range(idel-1):
