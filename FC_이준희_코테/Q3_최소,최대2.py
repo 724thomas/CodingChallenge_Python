bcb55b64082fb4cd0fe4fd2b11997d5213f9b711
ThomasChoi <724thomas@gmail.com>
2023-02-06 21:08:46 +0900

    230206

---
+++ FC_이준희_코테/Q3_최소,최대2.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/20065
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+
+3. 자료구조 :
+
+'''
+
+import sys
+for _ in range(int(input())):
+    input()
+    nums = sorted(list(map(int, sys.stdin.readline().split())))
+    print(nums[0], nums[-1])
