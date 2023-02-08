248fde6ecc51298c67d9e8044049915cd9756d93
ThomasChoi <724thomas@gmail.com>
2023-02-08 20:55:14 +0900

    230208

---
+++ FC_이준희_코테/230208/1541.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/1541
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+
+3. 자료구조 :
+
+'''
+
+
+import sys
+input = sys.stdin.readline
+
+Strings = input().strip().split('-')
+for i in range(len(Strings)):
+    Strings[i] = -sum(map(int, Strings[i].split('+')))
+print(-Strings[0] + sum(Strings[1:]))
