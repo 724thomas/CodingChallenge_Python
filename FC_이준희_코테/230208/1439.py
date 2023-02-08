3c74963f16814f38416279de878ceedf6321e500
ThomasChoi <724thomas@gmail.com>
2023-02-08 20:55:09 +0900

    230208

---
+++ FC_이준희_코테/230208/1439.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/1439
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+
+3. 자료구조 :
+
+'''
+
+
+import sys
+input = sys.stdin.readline
+
+String = input().strip()
+ans = String[0]
+for i in range(1, len(String) ):
+    if String[i -1] != String[i]:
+        ans += String[i]
+
+print(min(ans.count('0'), ans.count('1')))
