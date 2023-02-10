6c8830d4e2231ea185f8dba0308d9f588edff526
ThomasChoi <724thomas@gmail.com>
2023-02-10 19:40:42 +0900

    230210

--- FC_이준희_코테/230206/Q2_알람 시계.py
+++ FC_이준희_코테/230206/Q2_알람 시계.py
@@ -9,7 +9,7 @@
 
 '''
 
-
 import sys
-h , m = map(int, sys.stdin.readline().split())
-print(str(h) + " " + str(m-45) if m>=45 else "23" + " " + str(m+15) if h==0 else str(h-1) + " " +str(m+15))
+
+h, m = map(int, sys.stdin.readline().split())
+print(str(h) + " " + str(m - 45) if m >= 45 else "23" + " " + str(m + 15) if h == 0 else str(h - 1) + " " + str(m + 15))
