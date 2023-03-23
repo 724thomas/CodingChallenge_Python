b5177ae78a8504efca8ac5fc97dd9c0a808fd20e
ThomasChoi <724thomas@gmail.com>
2023-03-23 14:05:31 +0900

    230323

---
+++ LeetCode/2235AddTwoIntegers.py
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/add-two-integers/
+
+'''
+1. 아이디어 :
+    덧셈을 구현하면 된다.
+2. 시간복잡도 :
+    O(1)
+3. 자료구조 :
+    문자열
+'''
+
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
