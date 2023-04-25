6eeb968124f405a623c5c28602a242cee15666c5
ThomasChoi <724thomas@gmail.com>
2023-04-25 21:00:22 +0900

    230425

--- LeetCode/66PlusOne.py
+++
@@ -1,5 +0,0 @@
-#https://leetcode.com/problems/plus-one/
-
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        return [int(x) for x in list(str(int("".join([str(x) for x in digits]))+1))]
