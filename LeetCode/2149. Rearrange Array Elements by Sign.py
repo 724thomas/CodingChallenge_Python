5fe7fa79e66de22d6318686978ee404207d2e6a2
ThomasChoi <724thomas@gmail.com>
2023-04-26 20:50:53 +0900

    230426

--- LeetCode/2149. Rearrange Array Elements by Sign.py
+++ LeetCode/2149. Rearrange Array Elements by Sign.py
@@ -1,27 +1,26 @@
-#
+# https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
 
 '''
 1. 아이디어 :
-
+    정답으로 반환할 배열을 만들고, nums를 돌면서 짝수면 짝수번째 인덱스, 홀수면 홀수번째 인덱스에 넣는다.
 2. 시간복잡도 :
-
+    O(n) : n은 nums의 길이
 3. 자료구조 :
+    리스트
 
 '''
 
 
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        pos=[]
-        neg=[]
-        ans=[]
+        pos_idx=0
+        neg_idx=1
+        ans = [0 for x in range(len(nums))]
         for num in nums:
             if num>0:
-                pos.append(num)
+                ans[pos_idx]=num
+                pos_idx+=2
             else:
-                neg.append(num)
-
-        for i in range(len(pos)):
-            ans.append(neg.pop())
-            ans.append(pos.pop())
-        return ans[::-1]
+                ans[neg_idx]=num
+                neg_idx+=2
+        return ans
