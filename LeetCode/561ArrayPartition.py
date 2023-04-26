f19f16d97e660d7798c59130cfb4a1e33e5b2036
ThomasChoi <724thomas@gmail.com>
2023-04-26 20:50:35 +0900

    230426

--- LeetCode/561ArrayPartition.py
+++
@@ -1,9 +0,0 @@
-#https://leetcode.com/problems/array-partition/
-
-class Solution:
-    def arrayPairSum(self, nums: List[int]) -> int:
-        ans=0
-        nums=sorted(nums)
-        for i in range(0,len(nums),2):
-            ans+=nums[i]
-        return ans
