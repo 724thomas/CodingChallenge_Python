19ac6419e85d2751c52d436712ffe675c16d7b4e
724thomas <724thomas@gmail.com>
2023-07-03 23:50:20 +0900

    230703

--- ProgrammersSchool/기능개발.py
+++ ProgrammersSchool/기능개발.py
@@ -9,13 +9,15 @@
     1) 배열
 '''
 import math
+
+
 def solution(progresses, speeds):
-    answer, cmax, count = [], math.ceil((100 - progresses[0])/speeds[0]), 1
-    for i in range(1,len(progresses)):
-        if math.ceil((100 - progresses[i])/speeds[i])<=cmax:
-            count+=1
+    answer, cmax, count = [], math.ceil((100 - progresses[0]) / speeds[0]), 1
+    for i in range(1, len(progresses)):
+        if math.ceil((100 - progresses[i]) / speeds[i]) <= cmax:
+            count += 1
         else:
             answer.append(count)
-            cmax,count = math.ceil((100 - progresses[i])/speeds[i]),1
+            cmax, count = math.ceil((100 - progresses[i]) / speeds[i]), 1
     answer.append(count)
     return answer
