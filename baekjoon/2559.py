0f63a09478c8e53e13658ebc7acc30f047cfd9c5
724thomas <724thomas@gmail.com>
2023-07-15 09:27:11 +0900

    230715

--- baekjoon/2559.py
+++ baekjoon/2559.py
@@ -1,14 +1,22 @@
 # https://www.acmicpc.net/problem/2559
 
-import sys
+'''
+1. 아이디어 :
+    슬라이딩 윈도우 문제
+2. 시간복잡도 :
+    O(n)
+3. 자료구조 :
+    배열
+'''
 
-n, k = map(int, sys.stdin.readline().split())
-nums = list(map(int, sys.stdin.readline().split()))
-lp, rp = 0, k
+
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
+left, right = 0, k
 ans = cmax = sum(nums[:k])
-while rp < n:
-    ans = ans - nums[lp] + nums[rp]
+while right < n:
+    ans = ans - nums[left] + nums[right]
     cmax = max(ans, cmax)
-    lp += 1
-    rp += 1
+    left += 1
+    right += 1
 print(cmax)
