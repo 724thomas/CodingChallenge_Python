768283aeb653ebacc6b86b46e3ef2ede63742f83
724thomas <724thomas@gmail.com>
2023-07-20 00:38:57 +0900

    230720

--- baekjoon/2512.py
+++ baekjoon/2512.py
@@ -1,20 +1,42 @@
 # https://www.acmicpc.net/problem/2512
 
-import sys
+'''
+1. 아이디어 :
+    1) 이분탐색
+2. 시간복잡도 :
+    1) O(nlogn)
+3. 자료구조 :
+    1) 배열
+'''
 
-n = int(sys.stdin.readline())
-nums = list(map(int, sys.stdin.readline().split()))
-target = int(sys.stdin.readline())
-ans = [0] * n
-if sum(nums) <= target:
-    print(max(nums))
-    sys.exit()
-else:
-    start, end = 0, max(nums)
-    while start <= end:
-        mid = (start + end) // 2
-        if sum([i if i < mid else mid for i in nums]) <= target:
-            start = mid + 1
+n = int(input())
+req = list(map(int, input().split()))
+total = int(input())
+left = 1
+right = max(req)
+
+if sum(req) < total:
+    print(max(req))
+    exit()
+
+
+def check(money):
+    count = 0
+    for r in req:
+        if r > money:
+            count += money
         else:
-            end = mid - 1
-    print(end)
+            count += r
+    return count
+
+
+while left <= right:
+    mid = (left + right) // 2
+
+    temp = check(mid)
+    if temp > total:
+        right = mid - 1
+
+    elif temp <= total:
+        left = mid + 1
+print(right)
