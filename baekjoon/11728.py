e59416eb64847de27dee12f25c49e51d8142fa01
724thomas <724thomas@gmail.com>
2023-07-20 00:39:05 +0900

    230720

--- baekjoon/11728.py
+++ baekjoon/11728.py
@@ -1,24 +1,35 @@
 # https://www.acmicpc.net/problem/11728
 
-import sys
+'''
+1. 아이디어 :
+    투포인터. 두 배열을 정렬하고, 순서대로 하나씩 ans에 넣는다.
+    둘 중 하나라고 포인터가 넘어가면, 나머지 배열을 뒤에 붙인다.
+2. 시간복잡도 :
+    O(n+m)
+3. 자료구조 :
+    배열
+'''
 
-a, b = map(int, sys.stdin.readline().split())
-alist = list(map(int, sys.stdin.readline().split()))
-blist = list(map(int, sys.stdin.readline().split()))
+n, m = map(int, input().split())
+alist = list(map(int, input().split()))
+blist = list(map(int, input().split()))
+
 alist.sort()
 blist.sort()
-ans = []
-ap = 0
-bp = 0
-while ap<a and bp<b:
+
+ans, ap, bp = [], 0, 0
+
+while ap < n and bp < m:
     if alist[ap] > blist[bp]:
         ans.append(blist[bp])
-        bp+=1
+        bp += 1
     else:
         ans.append(alist[ap])
-        ap+=1
-if ap == a:
+        ap += 1
+
+if ap == n:
     ans.extend(blist[bp:])
 else:
     ans.extend(alist[ap:])
+
 print(' '.join(map(str, ans)))
