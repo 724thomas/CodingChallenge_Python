3661fa734119cd96bdfc437ef028b874946ee94b
724thomas <724thomas@gmail.com>
2023-07-22 22:50:15 +0900

    230722

--- baekjoon/2805.py
+++ baekjoon/2805.py
@@ -12,17 +12,14 @@
 import sys
 
 n, target = map(int, sys.stdin.readline().rstrip().split())
-alist = list(map(int, sys.stdin.readline().split()))
-lp, rp = 1, max(alist)
+tree = list(map(int, sys.stdin.readline().split()))
+lp, rp = 1, max(tree)
 
-while lp<=rp:
+while lp <= rp:
     cmin = (lp + rp) // 2
-    cut = sum([x-cmin for x in alist if x>cmin])
+    cut = sum([x - cmin for x in tree if x > cmin])
     if cut >= target:
-        lp = cmin+ 1
+        lp = cmin + 1
     else:
-        rp = cmin- 1
+        rp = cmin - 1
 print(rp)
-
-
-
