4c3172afcfd5ee74e0f769ee26ffd57c49191705
joon.pf <joon.pf@kakaobrain.com>
2023-08-05 03:42:19 +0900

    230805

--- LeetCode/56MergeIntervals.py
+++ LeetCode/56MergeIntervals.py
@@ -14,7 +14,6 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key = lambda x : x[0])
-        print(intervals)
         ans = [intervals[0]]
         for i in range(1, len(intervals)):
             if intervals[i][0] <= ans[-1][1]:
