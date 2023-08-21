f5255c16a0931cd12d7803680d04b15269826b4e
724thomas <724thomas@gmail.com>
2023-08-21 22:00:01 +0900

    230903

---
+++ LeetCode/2413. Smallest Even Multiple.py
@@ -0,0 +1,16 @@
+#
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+
+3. 자료구조 :
+
+'''
+
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n%2==0:
+            return n
+        return n*2
