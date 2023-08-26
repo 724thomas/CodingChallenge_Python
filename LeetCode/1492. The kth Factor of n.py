5f94aa414e85e57df47a849da22119ffaa16dfd7
724thomas <724thomas@gmail.com>
2023-08-26 22:00:01 +0900

    230903

---
+++ LeetCode/1492. The kth Factor of n.py
@@ -0,0 +1,20 @@
+#
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+
+3. 자료구조 :
+
+'''
+
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        for i in range(1,n+1):
+            if n%i==0:
+                k-=1
+                if k==0:
+                    return i
+
+        return -1
