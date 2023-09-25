9a99486193ee93c1f16826148b005bcd8a672665
724thomas <724thomas@gmail.com>
2023-09-25 16:05:50 +0900

    230925

--- baekjoon/1049.py
+++ baekjoon/1049.py
@@ -1,12 +1,12 @@
-# https://www.acmicpc.net/problem/1049
+# https://www.acmicpc.net/problem/
 
 '''
 1. 아이디어 :
-    6개씩 패키지 가격으로, 1개씩 패키지 가격으로 구매하는 것 중 최소값을 구하면 된다.
+
 2. 시간복잡도 :
-    O(n)
+
 3. 자료구조 :
-    배열
+
 '''
 
 n, m = map(int, input().split())
@@ -20,4 +20,5 @@ for _ in range(m):
 ans = [one]
 for i in range(1, n):
     ans.append(min(ans[i - 1] + one, six * ((i // 6) + 1)))
+
 print(ans[-1])
