118b1418c2f5fb2797c15caa52b4aad9cc45ed01
724thomas <724thomas@gmail.com>
2023-11-25 19:37:14 +0900

    231125

---
+++ ProgrammersSchool/숫자 문자열과 영단어.py
@@ -0,0 +1,17 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/81301
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+
+3. 자료구조 :
+
+'''
+
+def solution(s):
+    adict={"zero":0, "one":1, "two":2, "three":3, "four":4, "five":5, "six":6,
+           "seven":7, "eight":8, "nine":9}
+    for key in adict.keys():
+        s=s.replace(key,str(adict[key]))
+    return int(s)
