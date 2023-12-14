9410d4c71e184894deabeaf8469696a5122addf8
724thomas <724thomas@gmail.com>
2023-12-14 19:36:37 +0900

    231214

--- LeetCode/00Copy.py
+++ LeetCode/00Copy.py
@@ -8,3 +8,25 @@
 3. 자료구조 :
 
 '''
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+def ArrayToLinkedList(head):
+    if not head:
+        return None
+    node = ListNode(head[0])
+    node.next = createLinkedList(head[1:])
+    return node
+
+
+change_this = ArrayToLinkedList(change_this)
+
+
+
+
+head = [1,2,3,3,4,5]
+answer = [1,2,3]
+
