a7e1e5085a36cd9edfdaf57e242334f681a305dc
724thomas <724thomas@gmail.com>
2023-12-29 11:17:32 +0900

    231229

---
+++ baekjoon/25083.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/25083
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+
+3. 자료구조 :
+
+'''
+import sys
+
+input = sys.stdin.readline
+
+
+print("         ,r\'\"7")
+print("r`-_   ,\'  ,/")
+print(" \\. \". L_r\'")
+print("   `~\\/")
+print("      |")
+print("      |")
