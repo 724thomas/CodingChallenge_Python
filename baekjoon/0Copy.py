b58eaf67be89a801961450eab008de44e924bf97
724thomas <724thomas@gmail.com>
2024-01-01 01:49:32 +0900

    240101

--- baekjoon/0Copy.py
+++ baekjoon/0Copy.py
@@ -9,5 +9,24 @@
 
 '''
 import sys
+sys.setrecursionlimit(1000000)
 
 input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+n, m = list(map(int, input().split()))
+
+s = input().rstrip()
+
+dp = [[0 for j in range(n)] for i in range(n)]
+grid = [list(input().rstrip()) for _ in range(n)] # "aaa" "bbb"
+grid = list(list(map(int, input().split())) for _ in range(n)) # "0 0 0 0", "0 0 0 0"
+
+def solution(n, arr):
+
+
+    ans = 1
+    return ans
+
+print(solution(n, arr))
