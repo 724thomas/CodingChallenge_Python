40947d45e8c2e3568d60f5b9589cb3c3f955e0e6
724thomas <724thomas@gmail.com>
2024-01-02 01:15:26 +0900

    240102

---
+++ LeetCode/160. Intersection of Two Linked Lists -2.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+
+3. 자료구조 :
+
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        node_set = set()
+
+        while headA:
+            node_set.add(headA)
+            headA = headA.next
+
+        while headB:
+            if headB in node_set:
+                return headB
+            else:
+                headB = headB.next
