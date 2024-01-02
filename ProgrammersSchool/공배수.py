43479dc6760fc0ba0b050037e147c056458d167d
724thomas <724thomas@gmail.com>
2024-01-03 00:15:48 +0900

    240102

---
+++ ProgrammersSchool/공배수.py
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181936 공배수
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+
+3. 자료구조 :
+
+'''
+
+def solution(number, n, m):
+    return 1 if number%n ==0 and number%m==0 else 0
