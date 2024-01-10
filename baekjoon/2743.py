d211f59159c9acb4ca3a2a07ffd54e4dadd3bdd9
724thomas <724thomas@gmail.com>
2024-01-10 09:23:54 +0900

    240110

---
+++ baekjoon/2743.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/2743
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+
+3. 자료구조 :
+
+'''
+import sys
+sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
+print(len(input().rstrip()))
