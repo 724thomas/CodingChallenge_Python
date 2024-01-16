f51611414396463f5d5c4e313749e4164fc014cb
724thomas <724thomas@gmail.com>
2024-01-17 05:16:00 +0900

    240116

--- baekjoon/1697.py
+++ baekjoon/1697.py
@@ -1,18 +1,30 @@
-# https://www.acmicpc.net/problem/1697
+# https://www.acmicpc.net/problem/1697 숨바꼭질
 
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O( n )
+3. 자료구조 :
+
+'''
 import sys
+from collections import deque, defaultdict
 
-start, end = map(int, sys.stdin.readline().split())
-diff = abs(start - end)
-count = 0
-while diff != 0:
-    if diff % 2 == 0:
-        diff = diff // 2
-    else:
-        diff -=1
-    count+=1
-    print(diff, count)
+sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
 
-print(count)
+n, m = list(map(int, input().split()))
+queue = deque()
+queue.append(n)
+visited = defaultdict(int)
+while queue:
+    start = queue.popleft()
+    if start == m:
+        print(visited[start])
+        exit()
 
-
+    for cnext in (start - 1, start + 1, start * 2):
+        if 0 <= cnext <= 100001 and cnext not in visited:
+            visited[cnext] = visited[start]+1
+            queue.append(cnext)
