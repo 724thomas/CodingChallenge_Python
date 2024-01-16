f92480e956c18b5226ca84bbb9b943a7749da115
724thomas <724thomas@gmail.com>
2024-01-17 05:16:09 +0900

    240116

--- baekjoon/2239.py
+++ baekjoon/2239.py
@@ -1,25 +1,55 @@
-# https://www.acmicpc.net/problem/2239
+# https://www.acmicpc.net/problem/2239 스도쿠
 
 '''
 1. 아이디어 :
-    백트래킹으로 풀면된다.
+
 2. 시간복잡도 :
-    O(9!)
+    O(  )
 3. 자료구조 :
-    해시셋
+
 '''
-
-
 import sys
+sys.setrecursionlimit(1000000)
 input = sys.stdin.readline
 
-board = []
+n = 9
+grid = [list(map(int, input().rstrip())) for _ in range(9)]
+
+def solution(board):
+
+    def is_valid(row, col, num):
+        for i in range(9):
+            if board[row][i] == num:
+                return False
+            if board[i][col] == num:
+                return False
+
+        start_row = (row // 3) * 3
+        start_col = (col // 3) * 3
+        for i in range(3):
+            for j in range(3):
+                if board[start_row + i][start_col + j] == num:
+                    return False
+
+        return True
+
+    def backtrack():
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == 0:
+                    for num in range(1, 10):
+                        if is_valid(row, col, num):
+                            board[row][col] = num
+                            if backtrack():
+                                return True
+                            board[row][col] = 0
+                    return False
+        return True
+
+    backtrack()
+    return grid
+
+ans = solution(grid)
+
 for i in range(9):
-    nums = input().rstrip()
-    temp=[int(num) for num in nums]
-    board.append(temp)
-
-for row in board:
-    print(row)
-
-
+    print(*grid[i], sep="")
