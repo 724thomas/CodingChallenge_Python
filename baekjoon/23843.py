7f893d421fb1fe5cddd824c3c93a1950d4b913a1
724thomas <724thomas@gmail.com>
2024-01-29 00:19:20 +0900

    240128

--- baekjoon/23843.py
+++ baekjoon/23843.py
@@ -8,7 +8,6 @@
 3. 자료구조 :
 
 '''
-from collections import deque
 import heapq
 import sys
 
@@ -25,32 +24,29 @@ if n <= m:
 
 time = 0
 min_heap = []
-for i in range(m):
+while len(min_heap) < m:
     heapq.heappush(min_heap, devices.pop())
 
 while devices:
-    print(time)
-    print(min_heap)
-    print(devices)
-    time += heapq.heappop(min_heap)
-    while min_heap and min_heap[0] <= time:
-        time -= time - heapq.heappop(min_heap)
-    while len(min_heap) < m and devices:
-        heapq.heappush(min_heap, devices.pop())
+    temp = heapq.heappop(min_heap)
+    heapq.heappush(min_heap, devices.pop() + temp)
+while min_heap:
+    time = heapq.heappop(min_heap)
 
 print(time)
-print(min_heap)
-print(devices)
+
+
 '''
 8 4 4 1 1
 
 4 7
 4 1 1
 
-time = 4
-4 7 
+time = 7
+8 8
 1 1
 
 time = 
+
 
 '''
