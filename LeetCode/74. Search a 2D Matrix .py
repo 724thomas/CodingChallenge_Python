7e6b0fa8921958abf1c572f5cbc4be7e36ca7359
724thomas <724thomas@gmail.com>
2024-02-19 23:24:02 +0900

    240219

---
+++ LeetCode/74. Search a 2D Matrix .py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/search-a-2d-matrix/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(  )
+3. 자료구조 :
+
+'''
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        left = 0
+        right = len(matrix)-1
+
+        while left<=right:
+            mid = (left+right)//2
+            if matrix[mid][0] == target:
+                return True
+            elif matrix[mid][0] > target:
+                right = mid-1
+            elif matrix[mid][0] < target:
+                left = mid+1
+
+        matrix = matrix[right]
+
+        left = 0
+        right = len(matrix)-1
+        while left<=right:
+            mid = (left+right)//2
+            if matrix[mid] == target:
+                return True
+            elif matrix[mid] > target:
+                right = mid-1
+            elif matrix[mid] < target:
+                left = mid+1
+
+        return False
