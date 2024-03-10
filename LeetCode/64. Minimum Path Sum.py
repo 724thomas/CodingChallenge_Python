6a76caa170946eecf285fb34f8fb9e3f9a1188d1
724thomas <724thomas@gmail.com>
2024-03-10 22:07:32 +0900

    240310

---
+++ LeetCode/64. Minimum Path Sum.py
@@ -0,0 +1,23 @@
+#
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(  )
+3. 자료구조 :
+
+'''
+
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        for row in range(1, len(grid)):
+            grid[row][0] += grid[row-1][0]
+        for col in range(1, len(grid[0])):
+            grid[0][col] += grid[0][col-1]
+
+        for row in range(1, len(grid)):
+            for col in range(1, len(grid[0])):
+                grid[row][col] += min(grid[row-1][col], grid[row][col-1])
+        return grid[-1][-1]
