3c091651523b317787c5d7cad13a8d3f874de8fe
724thomas <724thomas@gmail.com>
2024-03-22 12:59:53 +0900

    240322

---
+++ LeetCode/128. Longest Consecutive Sequence.py
@@ -0,0 +1,27 @@
+#
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(  )
+3. 자료구조 :
+
+'''
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nset = set(nums)
+        cmax = 0
+        visited = set()
+
+        for n in nset:
+            if n not in visited:
+                i = 0
+                while n + i in nset:
+                    visited.add(n+i)
+                    i+=1
+                cmax = max(cmax, i)
+        return cmax
+
