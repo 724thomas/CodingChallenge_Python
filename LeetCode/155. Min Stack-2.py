31f2eed1bfe3ae5f2c10f24bcbe559511b4210a0
724thomas <724thomas@gmail.com>
2024-06-07 01:05:07 +0900

    240607

---
+++ LeetCode/155. Min Stack-2.py
@@ -0,0 +1,46 @@
+#
+
+'''
+1. 아이디어 :
+    스택과 모노토닉 스택 사용
+2. 시간복잡도 :
+    O( 1 )
+3. 자료구조 :
+    스택
+'''
+
+
+from collections import defaultdict
+class MinStack:
+
+    def __init__(self):
+        self.min_stack = []
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+
+        if self.min_stack and self.min_stack[-1] == val:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
