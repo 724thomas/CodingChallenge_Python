e8c66566d1d421f66bc82aa2cfe85462b4ee181b
724thomas <724thomas@gmail.com>
2024-06-24 10:14:48 +0900

    240624

---
+++ LeetCode/235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,32 @@
+#
+
+'''
+1. 아이디어 :
+    이진탐색트리이므로 dfs를 쓸 필요가 없다.
+2. 시간복잡도 :
+    O( n )
+3. 자료구조 :
+    -
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val > q.val:
+            p, q = q, p
+
+        curr = root
+        while curr:
+            if p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            elif p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            else:
+                return curr
