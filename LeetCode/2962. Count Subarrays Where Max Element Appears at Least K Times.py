5398cc2c3a4bf61323eef227dc4fa648e35ae5d3
724thomas <724thomas@gmail.com>
2024-08-20 07:22:33 +0900

    240820

---
+++ LeetCode/2962. Count Subarrays Where Max Element Appears at Least K Times.py
@@ -0,0 +1,30 @@
+#
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 :
+
+'''
+
+
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        max_num = max(nums)
+        count = 0
+        left = 0
+        max_count = 0
+
+        for right in range(len(nums)):
+            if nums[right] == max_num:
+                max_count += 1
+
+            while max_count >= k:
+                count += len(nums) - right
+                if nums[left] == max_num:
+                    max_count -= 1
+                left += 1
+
+        return count
