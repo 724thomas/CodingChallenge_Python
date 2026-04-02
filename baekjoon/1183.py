76c08805fef9cc30e79477b04cf659bcd4107e10
724thomas <724thomas@gmail.com>
2026-04-03 00:31:14 +0900

    1183

---
+++ baekjoon/1183.py
@@ -0,0 +1,50 @@
+# https://www.acmicpc.net/problem/
+
+'''
+1. 아이디어 :
+
+2. 시간복잡도 :
+    O(
+3. 자료구조 / 알고리즘 :
+
+'''
+
+
+import sys
+#sys.setrecursionlimit(1000000)
+# input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
+
+def solution(n, arr):
+    arr.sort()
+    print(*arr)
+
+    if n%2 == 1:
+        return 1
+
+    return arr[n//2] - arr[n//2 - 1] + 1
+
+
+if __name__ == '__main__':
+    n = int(input().rstrip())
+    arr = []
+    for _ in range(n):
+        A, B = map(int, input().split())
+        arr.append(B-A)
+    print(solution(n, arr))
+
+# n = int(input().rstrip())
+#
+# n, m = map(int, input().split())
+# n, m = list(map(int, input().split()))
+# a = [c for c in input().strip()]
+#
+# s = input().rstrip()
+
+# arr = list(map(int, input().strip().split()))
+# arr = tuple(map(int, input().split()))
+# integer_list = [int(num) for num in input().split()]
+# dp = [[0 for _ in range(n)] for _ in range(n)]
+# dp = [[0 for j in range(n)] for i in range(n)]
+# grid = [list(input().rstrip()) for _ in range(n)] # "aaa" "bbb"
+# grid = list(list(map(int, input().split())) for _ in range(n)) # "0 0 0 0", "0 0 0 0"
